6a932c4a7d01ee2ecdfb4cb5dda5bc2d0f8d7fa3
Izaak Schroeder <izaak.schroeder@gmail.com>
2016-01-21 23:33:44 -0800

    Speedup `hapi` and `express` connections.
    By hoisting the middleware generation function and (cleverly) attatching the necessary data to the request, modest performance benefits can be seen.

--- src/adapter/express.js
+++ src/adapter/express.js
@@ -1,10 +1,16 @@
-
 export default function express(createMiddleware) {
+  const middleware = createMiddleware({
+    error: (err, req) => {
+      if (req) {
+        req.__next(err);
+      }
+    },
+    request: (req) => {
+      req.__next();
+    },
+  });
   return (req, res, next) => {
-    const middleware = createMiddleware({
-      error: next,
-      request: () => next(),
-    });
+    req.__next = next;
     middleware.request(req, res);
   };
 }
--- src/adapter/hapi.js
+++ src/adapter/hapi.js
@@ -1,13 +1,17 @@
-
 export default function hapi(createMiddleware) {
+  const middleware = createMiddleware({
+    request: (req) => req.__reply.continue(),
+    error: (err, req) => {
+      if (req) {
+        req.__reply(err);
+      }
+    },
+  });
   return {
     type: 'onRequest',
     method: (request, reply) => {
       const { req, res } = request.raw;
-      const middleware = createMiddleware({
-        request: () => reply.continue(),
-        error: (err) => reply(err),
-      });
+      req.__reply = reply;
       middleware.request(req, res);
     },
   };
