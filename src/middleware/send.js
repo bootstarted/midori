de0a862eb9ff29392114f916774ec1ff5c449500
Neal Granger <neal@metalab.co>
2016-01-23 20:09:42 -0800

    Add option to set `Content-Type` header to `send`.
    Breaking change to `send` arguments.

--- src/middleware/send.js
+++ src/middleware/send.js
@@ -1,11 +1,17 @@
 import isFunction from 'lodash/isFunction';
 
-export default (body = (req) => req.body) => (app) => {
+export default ({
+  body = (req) => req.body,
+  type = () => 'text/html; charset=utf-8',
+} = {}) => (app) => {
   const getBody = isFunction(body) ? body : () => body;
+  const getType = isFunction(type) ? type : () => type;
 
   return {
     ...app,
     request(req, res) {
+      const contentType = getType(req);
+      contentType && res.setHeader('Content-Type', getType(req));
       res.end(getBody(req));
     },
   };
