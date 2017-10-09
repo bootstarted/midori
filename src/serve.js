24df1012a78810e1315613d5fe849816b5b41776
Izaak Schroeder <izaak.schroeder@gmail.com>
2017-10-08 20:19:18 -0700

    Allow resuming request pipeline when file not found in `serve`.
    This allows for doing other processing instead of just sending back the 404 response. Additionally, error handling has been brought back and any errors encountered in `serve` will be propagated into the pipeline.

--- src/serve.js
+++ src/serve.js
@@ -3,9 +3,29 @@ import send from 'send';
 import parse from 'parseurl';
 import request from './request';
 import pure from './pure';
+import next from './next';
+import compose from './compose';
+import status from './status';
+import write from './send';
 
 import type {AppCreator} from './types';
 import type {IncomingMessage} from 'http';
+type Options = {
+  final: boolean,
+  onDirectory: (directory: string) => AppCreator,
+  acceptRanges: ?boolean,
+  cacheControl: ?boolean,
+  dotfiles: ?('allow' | 'deny' | 'ignore'),
+  end: ?number,
+  etag: ?boolean,
+  extensions: ?Array<string>,
+  immutable: ?boolean,
+  index: ?boolean,
+  lastModified: ?boolean,
+  maxAge: ?(number | string),
+  root: string,
+  start: ?number,
+};
 
 const getBase = (req: IncomingMessage): string => {
   const url: URL = parse(req);
@@ -17,16 +37,38 @@ const getBase = (req: IncomingMessage): string => {
 
 /**
  * Server static files with `send`.
- * @param {Object} options Options to pass through to `send`.
+ * @param {Object} options Options to pass through to `send`. You can see then
+ * list of parameters here: https://www.npmjs.com/package/send
+ * @param {Boolean} options.final True to terminate request handling here. You
+ * can set this to `false` if you want to keep processing the app chain if a
+ * file is not found.
+ * @param {Function} options.onDirectory Invoked with a directory path when a
+ * directory is encountered. You can use this to do things like provide a
+ * directory listing or return some other status. Defaults to returning 204.
  * @returns {Function} App creator.
  */
-export default (options: Object): AppCreator => request((req, res) => {
+export default ({
+  final = true,
+  onDirectory = () => compose(status(204), write('')),
+  ...options
+}: Options): AppCreator => request((req, res) => {
   const path = getBase(req);
-  const stream = send(req, path, options);
-  stream
-    // .on('error', (err) => app.error(err, req, res))
-    // .on('directory', redirect)
-    // .on('headers', headers)
-    .pipe(res);
-  return pure(null);
+  return new Promise((resolve, reject) => {
+    const stream = send(req, path, options);
+    stream
+      .on('error', (err) => {
+        if (err && err.code === 'ENOENT' && !final) {
+          resolve(next);
+        } else {
+          reject(err);
+        }
+      })
+      .on('directory', (res, path) => {
+        resolve(onDirectory(path));
+      })
+      .pipe(res)
+      .on('finish', () => {
+        resolve(pure(null));
+      });
+  });
 });
--- test/spec/serve.spec.js
+++ test/spec/serve.spec.js
@@ -1,51 +1,101 @@
+import {expect} from 'chai';
 import sinon from 'sinon';
 import bl from 'bl';
-import onFinished from 'on-finished';
+import {resolve} from 'path';
 
 import serve from '../../src/serve';
+import send from '../../src/send';
 
 describe('serve', () => {
-  it('should serve some files', (done) => {
-    const spy = sinon.spy();
-    const getHeader = sinon.stub().returns('');
-    const setHeader = sinon.stub();
-    const stream = bl(() => {});
-    const app = serve({root: __dirname})({
-      request: spy,
-      error: done,
+  let getHeader;
+  let setHeader;
+  let stream;
+  let result;
+  let nextApp;
+
+  beforeEach(() => {
+    getHeader = sinon.stub().returns('');
+    setHeader = sinon.stub();
+    result = new Promise((resolve, reject) => {
+      nextApp = {
+        request: () => resolve(null),
+        error: reject,
+      };
+      stream = bl((err, result) => {
+        err ? reject(err) : resolve(result);
+      });
     });
+    stream.finished = false;
     stream.getHeader = getHeader;
     stream.setHeader = setHeader;
-    onFinished(stream, (err) => {
-      // FIXME: This appears to be called out of order...
-      // expect(stream.setHeader).to.be.called;
-      done(err);
-    });
-    app.request({url: '/serve.spec.js', headers: {}, method: 'GET'}, stream);
   });
 
-  it('should work with path prefixes', (done) => {
-    const spy = sinon.spy();
-    const getHeader = sinon.stub().returns('');
-    const setHeader = sinon.stub();
-    const stream = bl(() => {});
-    const app = serve({root: __dirname})({
-      request: spy,
-      error: done,
+  it('should serve some files', () => {
+    const app = serve({root: __dirname})();
+    app.request({url: '/serve.spec.js', headers: {}, method: 'GET'}, stream);
+    return result.then((data) => {
+      expect(data.toString()).to.contain('import bl');
     });
-    stream.getHeader = getHeader;
-    stream.setHeader = setHeader;
-    // fs.createReadStream('./base.js').pipe(stream);
+  });
+
+  it('should work with path prefixes', () => {
+    const app = serve({root: __dirname})(nextApp);
     app.request({
       url: '/foo/serve.spec.js',
       path: '/foo',
       headers: {},
       method: 'GET',
     }, stream);
-    onFinished(stream, (err) => {
-      // FIXME: This appears to be called out of order...
-      // expect(stream.setHeader).to.be.called;
-      done(err);
+    return result.then((data) => {
+      expect(data.toString()).to.contain('import bl');
+    });
+  });
+
+  it('should call next handler with `final` set to `false`', () => {
+    const app = serve({root: __dirname, final: false})(nextApp);
+    app.request({url: '/404', headers: {}, method: 'GET'}, stream);
+    return result.then((data) => {
+      expect(data).to.be.null;
+    });
+  });
+
+  it('should invoke directory handler on directories', () => {
+    const spy = sinon.spy();
+    const app = serve({
+      root: __dirname,
+      index: false,
+      onDirectory: (path) => {
+        spy(resolve(path));
+        return send('');
+      },
+    })(nextApp);
+    app.request({url: '/', headers: {}, method: 'GET'}, stream);
+    return result.then((data) => {
+      expect(data.length).to.equal(0);
+      expect(spy).to.be.calledOnce;
+    });
+  });
+
+  it('should by default return `204` on directories', () => {
+    const app = serve({
+      root: __dirname,
+      index: false,
+    })(nextApp);
+    app.request({url: '/', headers: {}, method: 'GET'}, stream);
+    return result.then((data) => {
+      expect(data.length).to.equal(0);
+      expect(stream.statusCode).to.equal(204);
+    });
+  });
+
+  it('should invoke error handler on errors', () => {
+    const app = serve({root: __dirname})(nextApp);
+    app.request({url: '/404', headers: {}, method: 'GET'}, stream);
+    return result.then(() => {
+      throw new Error();
+    }, (err) => {
+      expect(err).to.have.property('statusCode', 404);
+      return Promise.resolve();
     });
   });
 });
